4a26da1e079d35c8ab4ef5428f73305536fd6640
Jonah <jonah_lefkoff@hotmail.com>
2021-03-11 10:26:22 -0700

    remove api key

--- public/app.js
+++ public/app.js
@@ -9,14 +9,18 @@
         storageBucket: "lidarspeedometer.appspot.com",
       };
       firebase.initalizeApp(config);
+      const database = firebase.database()
+      var ref = firebase.database().ref();                           
+      ref.on("value", function(snapshot){
+        output.innerHTML = JSON.stringify(snapshot.val(), null, 2);
+      });
+    //   //get elements
+    //   const preObject = document.getElementById('object');
 
-      //get elements
-      const preObject = document.getElementById('object');
+    // // create references
+    // const dbRefObject = firebase.database().ref().child('object');
 
-    // create references
-    const dbRefObject = firebase.database().ref().child('object');
-
-    //sync objects
-    dbRefObject.on('value', snap => console.log(snap.val()));
+    // //sync objects
+    // dbRefObject.on('value', snap => console.log(snap.val()));
 
 }());
